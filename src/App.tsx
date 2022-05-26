34a83dca618b710e7186c6e57e131f5e608bd558
Bruno Perkovic <brunko10@gmail.com>
2022-05-26 21:09:32 +0200

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import { Blogpost } from './components/Blogpost';
-import Header from "./components/Header/Header"
-
-
+import React from "react";
+import { Blogpost } from "./components/Blogpost";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-   
     <>
-    <h2 className="text-4xl">Neki naslov</h2>
-    <div className="max-w-4xl mx-auto flex flex-wrap">
-    <Header />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-        <Blogpost className="md:w-1/2  p-3" text='AAAAAAAAAAAA' />
-    </div>
+      <h2 className="text-4xl">Neki naslov</h2>
+      <div className="max-w-4xl mx-auto flex flex-wrap">
+        <Header />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+        <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
+      </div>
+      <Footer />
     </>
   );
 }
