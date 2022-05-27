6f3b9f326a2a56a9c2400f6e2b01e3a314906bac
markokova16 <markokovac1696@gmail.com>
2022-05-27 10:29:49 +0200

    Footer gotov(?)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,25 +3,29 @@ import React from "react";
 const Footer = () => {
   return (
     <div className=" flex flex-col  justify-center items-center bg-black text-white w-screen overflow-x-hidden">
-      <div className="flex justify-between">
+      <div className="flex justify-between space-x-10 tracking-widest overflow-x-hidden">
         <div className="font-bold">DIGITAL PRODUCT DESIGN</div>
         <div>REMOTE WORK</div>
         <div className="font-bold">UX DESIGN</div>
         <div>DISTRIBUTED TEAMS</div>
         <div className="font-bold">CREATIVITY</div>
         <div>STRATEGY</div>
+        <div className="font-bold">SUSPENSE</div>
+        <div>GROWTH</div>
         <div className="font-bold">SUPPORT 24/7</div>
-        <div>AGILE</div>
+        <div>MAGIC</div>
+        <div className="font-bold">IMPROVEMENT</div>
+        <div>CLASS</div>
       </div>
 
       <div className="my-5 flex justify-center items-center">NORDIC ROSE</div>
-      <div>
+      <div className="text-xs text-center ml-10 mr-10 overflow-x-hidden ">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
         reiciendis facere debitis similique quidem? Ad temporibus quasi cumque
         amet error?
       </div>
 
-      <div className="flex justify-between items-center my-5 underline">
+      <div className="flex  my-5 underline">
         <div>Twitter</div>
         <div className="mx-5">Linkedin</div>
         <div>RSS</div>
