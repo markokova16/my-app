211c42695f462ee8ce8103301cc2caf6b888ff78
markokova16 <markokovac1696@gmail.com>
2022-05-29 21:40:32 +0200

    Male promjene

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ const Nav = () => {
     setOpen(!open);
   };
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
+    <div className="shadow-md w-full sticky top-0 left-0">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
         <div className="text-4xl font-bold cursor-pointer flex items-center font-[Poppins]">
           Nordic Rose
