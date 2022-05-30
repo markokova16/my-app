af2d869ef6cccab5458146adf64d908191eeda3b
markokova16 <markokovac1696@gmail.com>
2022-05-30 15:59:05 +0200

    Zavrsen higlighted article potencijalno

--- src/components/HighlightedArticle/HighlightedArticle.tsx
+++ src/components/HighlightedArticle/HighlightedArticle.tsx
@@ -6,14 +6,14 @@ const HighlightedArticle = () => {
     <div className="flex flex-col items-center  mt-10 ">
       <Image className="w-1/2" />
 
-      <div className="flex text-center font-bold items text-5xl mt-5">
+      <div className="w-1/2 flex text-center font-bold items text-4xl mt-5">
         <h1>
           A few words about this blog platform, Ghost, and how this site was
           made
         </h1>
       </div>
 
-      <div className="mt-8 mb-4 border-b-2 border-black  ">
+      <div className="mt-8 mb-4 border-b-2 border-black w-80 text-center">
         <p>
           Why Ghost (& Figma) instead of Medium, WordPress or other options?
         </p>
