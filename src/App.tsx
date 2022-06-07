bd6f3e8a92f38ea787a5fdb026d0a12130aded42
Bruno Perkovic <brunko10@gmail.com>
2022-06-07 14:29:06 +0200

    nista ne radi

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Blogpost } from "./components/Blogpost";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HighlightedArticle from "./components/HighlightedArticle/HighlightedArticle";
 import ArticlePost from "./pages/ArticlePost";
-import { getPosts } from "./services/getPosts";
+import { getPosts, IPost } from "./services/getPosts";
 
 function App() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
   useEffect(() => {
-    getPosts().then((response) => console.log(response));
+    getPosts().then((response) => {
+      setPosts(response.data);
+    });
   }, []);
+
   return (
     <>
       <Header />
-      <ArticlePost />
-      {/*       <HighlightedArticle />
 
       <h2 className="text-4xl font-bold flex justify-center border-t-2 border-black">
         All articles
       </h2>
 
-      {
-        <div className="w-full mx-auto flex flex-wrap max-w-xl ">
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-          <Blogpost className="md:w-1/2  p-3" text="AAAAAAAAAAAA" />
-        </div>
-      } */}
+      <div className="w-full mx-auto flex flex-wrap max-w-xl ">
+        {posts.map((post, i) => {
+          return (
+            <Blogpost
+              text={post.title.rendered}
+              key={i}
+              img={post._embedded["wp:featuredmedia"][0].link}
+            />
+          );
+        })}
+      </div>
       <Footer />
     </>
   );
--- src/services/getPosts.ts
+++ src/services/getPosts.ts
@@ -4,13 +4,16 @@ export interface IPost {
   title: { rendered: string };
   content: { rendered: string };
   excerpt: { rendered: string };
-  "wp:featuredmedia": { link: string };
+  _embedded: {
+    "wp:featuredmedia": { link: string }[];
+
+  };
 }
 
 export const getPosts = async () => {
   try {
     const res = await axios.get<IPost[]>(
-      `http://locblog.local/wp-json/wp/v2/posts?_embed`
+      `http://locasticblog.local/wp-json/wp/v2/posts?_embed`
     );
     return res;
   } catch (error) {
